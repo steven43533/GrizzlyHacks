1e2197a50e066d9418d87b2dd03b1cdc4b2974da
ddavis60 <ddavis60@ggc.edu>
2020-02-27 17:34:09 -0500

    fighting with angular and bootstrap

--- WebPage/GGCHacks/src/app/app.module.ts
+++ WebPage/GGCHacks/src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { UserdashboardComponent } from './userdashboard/userdashboard.component';
+import { AdmindashboardComponent } from './admindashboard/admindashboard.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'prefix'},
+  { path: 'home', component: LandingpageComponent },
+  { path: 'user-dashboard', component: UserdashboardComponent},
+  { path: 'admin-dashboard', component: AdmindashboardComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
+    UserdashboardComponent,
+    AdmindashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot([
+      { path: '', component: LandingpageComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
