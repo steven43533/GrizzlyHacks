ed5a28c47add6e8124176fb59f56112495c568be
Josh <joshbeers999@gmail.com>
2020-04-20 18:52:14 -0400

    fixed a routing issue and a undefied error

--- WebPage/GGCHacks/src/app/guards/user-guard.guard.ts
+++ WebPage/GGCHacks/src/app/guards/user-guard.guard.ts
@@ -13,7 +13,7 @@ export class UserGuardGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if ( this.auth.afAuth.user && this.auth.verified) {
+    if ( this.auth.afAuth.user) {
       return true;
     }
     this.router.navigate(['/home']);
