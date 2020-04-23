43ac3068f50511b76077e1a89c7a8260b46fe11e
Josh <joshbeers999@gmail.com>
2020-04-23 03:08:00 -0400

    web hosting accomplished

--- WebPage/GGCHacks/src/environments/environment.prod.ts
+++ WebPage/GGCHacks/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyDC0UftD4fma1ZnN9vM-AZm-d0Q4uGkhiE',
+    authDomain: 'grizzly-hacks.firebaseapp.com',
+    databaseURL: 'https://grizzly-hacks.firebaseio.com',
+    projectId: 'grizzly-hacks',
+    storageBucket: 'grizzly-hacks.appspot.com',
+    messagingSenderId: '275728046577',
+    appId: '1:275728046577:web:8e659b704e864338d24c50',
+    measurementId: 'G-MTFTJ92WQS'
+  }
 };
--- WebPage/GGCHacks/src/environments/environment.ts
+++ WebPage/GGCHacks/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase : {
     apiKey: 'AIzaSyDC0UftD4fma1ZnN9vM-AZm-d0Q4uGkhiE',
     authDomain: 'grizzly-hacks.firebaseapp.com',
