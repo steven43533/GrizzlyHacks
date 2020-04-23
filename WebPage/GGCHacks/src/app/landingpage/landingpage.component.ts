a674c358035334eb13caeb29ea0dc22ad3d98aaa
ddavis60 <ddavis60@ggc.edu>
2020-04-22 22:40:34 -0400

    fixed email page

--- WebPage/GGCHacks/src/app/landingpage/landingpage.component.ts
+++ WebPage/GGCHacks/src/app/landingpage/landingpage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-landingpage',
