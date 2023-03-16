0dad7c790f6afdd02ef54d198f88adb3c5edbbce
Jordan Moats <jmoats@ggc.edu>
2023-03-16 00:21:22 -0400

    refactors multiple files to correct names

--- WebPage/GGCHacks/src/app/UserStuff/verify-email-user/verify-email-user.component.spec.ts
+++ WebPage/GGCHacks/src/app/UserStuff/verify-email-user/verify-email-user.component.spec.ts
@@ -1,17 +1,23 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { VerifyEmailUserComponent } from './verify-email-user.component';
+import { AuthService } from '../../services/auth.service';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../../../environments/environment';
 
 describe('VerifyEmailUserComponent', () => {
   let component: VerifyEmailUserComponent;
   let fixture: ComponentFixture<VerifyEmailUserComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ VerifyEmailUserComponent ]
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+      ],
+      declarations: [ VerifyEmailUserComponent ],
+      providers: [ AuthService ]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(VerifyEmailUserComponent);
@@ -22,4 +28,5 @@ describe('VerifyEmailUserComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
