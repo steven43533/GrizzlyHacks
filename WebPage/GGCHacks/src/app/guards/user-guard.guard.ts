cf517df04cc6d3da637ab57366ddcb81c843c28a
Jordan Moats <jmoats@ggc.edu>
2023-03-16 00:20:33 -0400

    updates AdminGuard and UserGuard to use non-deprecated method canActivate ()

--- WebPage/GGCHacks/src/app/guards/user-guard.guard.ts
+++ WebPage/GGCHacks/src/app/guards/user-guard.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import {AuthService} from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserGuardGuard implements CanActivate {
+export class UserGuardGuard{
 
   constructor(private auth: AuthService, private router: Router) {}
 
