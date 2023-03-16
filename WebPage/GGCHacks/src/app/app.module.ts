63218f2469f971adf09b96989485ef46eed21e48
Jordan Moats <jmoats@ggc.edu>
2023-03-16 11:45:51 -0400

    refactors imports and removes redundant fire-base-stuff.module

--- WebPage/GGCHacks/src/app/app.module.ts
+++ WebPage/GGCHacks/src/app/app.module.ts
@@ -1,15 +1,23 @@
+//Module Imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { LandingpageComponent } from './landingpage/landingpage.component';
-import { AdmindashboardComponent } from './admindashboard/admindashboard.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './modules/app-routing.module';
 
-import { FireBaseStuffModule } from './modules/fire-base-stuff.module';
+
+//FireBase Imports
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule} from '@angular/fire/compat/storage';
+import {environment} from '../environments/environment';
+
+//Components
+import { AppComponent } from './app.component';
 import { CreateAccountComponent } from './UserStuff/create-account/create-account.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './UserStuff/login/login.component';
 import { VerifyEmailComponent } from './email-action/verify-email/verify-email.component';
 import { HeaderComponent } from './header/header.component';
@@ -20,8 +28,10 @@ import { ApplicationComponent } from './application/application.component';
 import { UserQueryPipe } from './admindashboard/user-query.pipe';
 import { CheckFilerPipe } from './admindashboard/check-filer.pipe';
 import { EventCalanderComponent } from './EventCalanderStuff/event-calander/event-calander.component';
-import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { VerifyEmailUserComponent } from './UserStuff/verify-email-user/verify-email-user.component';
+import { AdmindashboardComponent } from './admindashboard/admindashboard.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+
 
 
 @NgModule({
@@ -46,7 +56,11 @@ import { VerifyEmailUserComponent } from './UserStuff/verify-email-user/verify-e
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    FireBaseStuffModule,
+    CommonModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
     FormsModule,
     ReactiveFormsModule,
     NgbModule
