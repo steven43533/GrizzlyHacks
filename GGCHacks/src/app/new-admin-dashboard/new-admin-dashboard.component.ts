164008d5867997df5fd02077e8eba6f330f73fe0
supersand24 <supersand24@gmail.com>
2023-03-27 21:47:10 -0400

    Designed New Admin Dashboard

--- GGCHacks/src/app/new-admin-dashboard/new-admin-dashboard.component.ts
+++ GGCHacks/src/app/new-admin-dashboard/new-admin-dashboard.component.ts
@@ -3,7 +3,6 @@ import { User } from '../interfaces/user';
 import { Subscription } from 'rxjs';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
 import { AuthService } from '../services/auth.service';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-admin-dashboard',
@@ -16,7 +15,9 @@ export class NewAdminDashboardComponent {
   fullUserArray: User[];
   sub: Subscription;
   search: string;
-  currentFilter: string = 'All';
+  currentFilter: string = 'Non-Admin';
+  checkboxHasApplication: boolean;
+  checkboxAcceptedToHackathon: boolean;
 
   constructor(private afs: AngularFirestore, public auth: AuthService) { 
 
@@ -40,29 +41,10 @@ export class NewAdminDashboardComponent {
     this.sub.unsubscribe();
   }
 
-  makeSuperAdmin(user: User) {
-    if (confirm(`Are you sure you want to make ${user.firstName} ${user.lastName} a super admin?`)) {
-      const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
-      user.isSuperAdmin = true;
-      userRef.update(user);
-      }
-  }
-  takeSuperAdmin(user: User) {
-    if (user.email === this.auth.user.email) {
-      alert('You cannot take your own super admin status.');
-      return;
-    }
-    if (confirm(`Are you sure you want to take ${user.firstName} ${user.lastName}'s super admin status?`)) {
-      const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
-      user.isSuperAdmin = false;
-      userRef.update(user);
-    }
-  }
-
   makeAdmin(user: User) {
     if (confirm(`Are you sure you want to make ${user.firstName} ${user.lastName} an admin?`)) {
       const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
-      user.isAdmin = true;
+      user.adminLevel = 1;
       userRef.update(user);
     }
   }
@@ -70,14 +52,34 @@ export class NewAdminDashboardComponent {
   takeAdmin(user: User) {
     if (confirm(`Are you sure you want to take ${user.firstName} ${user.lastName}'s admin status?`)) {  
       const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
-      user.isAdmin = false;
+      user.adminLevel = 0;
+      userRef.update(user);
+    }
+  }
+
+  makeSuperAdmin(user: User) {
+    if (confirm(`Are you sure you want to make ${user.firstName} ${user.lastName} a super admin?`)) {
+      const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+      user.adminLevel = 2;
+      userRef.update(user);
+      }
+  }
+
+  takeSuperAdmin(user: User) {
+    if (user.email === this.auth.user.email) {
+      alert('You cannot take your own super admin status.');
+      return;
+    }
+    if (confirm(`Are you sure you want to take ${user.firstName} ${user.lastName}'s super admin status?`)) {
+      const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+      user.adminLevel = 1;
       userRef.update(user);
     }
   }
 
   filterUsers(filter: string) {
-    if (filter === 'All') {
-      this.currentFilter = 'All';
+    if (filter === 'Non-Admin') {
+      this.currentFilter = 'Non-Admin';
       this.users = this.fullUserArray;
     }
     if (filter === 'Admin') {
@@ -87,6 +89,22 @@ export class NewAdminDashboardComponent {
     if (filter === 'Super Admin') {
       this.currentFilter = 'Super Admin';
       this.users = this.fullUserArray.filter( u => u.isSuperAdmin);
+    }
+  }
+
+  toggleHasApplicationCheckbox() {
+    if (this.checkboxHasApplication) {
+      this.checkboxHasApplication = false;
+    } else {
+      this.checkboxHasApplication = true;
+    }
+  }
+
+  toggleAcceptedToHackathon() {
+    if (this.checkboxAcceptedToHackathon) {
+      this.checkboxAcceptedToHackathon = false;
+    } else {
+      this.checkboxAcceptedToHackathon = true;
     }
   }
 }
