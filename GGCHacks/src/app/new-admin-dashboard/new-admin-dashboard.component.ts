5f2b2a4937f5abf00cf6a5ec1d055f3948f62668
supersand24 <supersand24@gmail.com>
2023-03-28 10:31:55 -0400

    Filters now properly filter

--- GGCHacks/src/app/new-admin-dashboard/new-admin-dashboard.component.ts
+++ GGCHacks/src/app/new-admin-dashboard/new-admin-dashboard.component.ts
@@ -15,9 +15,9 @@ export class NewAdminDashboardComponent {
   fullUserArray: User[];
   sub: Subscription;
   search: string;
-  currentFilter: string = 'Non-Admin';
-  checkboxHasApplication: boolean;
-  checkboxAcceptedToHackathon: boolean;
+  adminFilter: string = 'All Members';
+  applicationFilter: string = 'All';
+  acceptedFilter: string = 'All';
 
   constructor(private afs: AngularFirestore, public auth: AuthService) { 
 
@@ -77,34 +77,92 @@ export class NewAdminDashboardComponent {
     }
   }
 
-  filterUsers(filter: string) {
+  changeAdminFilter(filter: string) {
+    if (filter === 'All Members') {
+      this.adminFilter = 'All Members';
+    }
     if (filter === 'Non-Admin') {
-      this.currentFilter = 'Non-Admin';
-      this.users = this.fullUserArray;
+      this.adminFilter = 'Non-Admin';
     }
     if (filter === 'Admin') {
-      this.currentFilter = 'Admin';
-      this.users = this.fullUserArray.filter( u => u.isAdmin);
+      this.adminFilter = 'Admin';
     }
     if (filter === 'Super Admin') {
-      this.currentFilter = 'Super Admin';
-      this.users = this.fullUserArray.filter( u => u.isSuperAdmin);
+      this.adminFilter = 'Super Admin';
     }
+    this.filterUserEntries();
   }
 
-  toggleHasApplicationCheckbox() {
-    if (this.checkboxHasApplication) {
-      this.checkboxHasApplication = false;
-    } else {
-      this.checkboxHasApplication = true;
+  changeApplicationFilter(filter: string) {
+    if (filter === 'All') {
+      this.applicationFilter = 'All';
     }
+    if (filter === 'Has Application') {
+      this.applicationFilter = 'Has Application';
+    }
+    if (filter === 'Missing Application') {
+      this.applicationFilter = 'Missing Application';
+    }
+    this.filterUserEntries();
   }
 
-  toggleAcceptedToHackathon() {
-    if (this.checkboxAcceptedToHackathon) {
-      this.checkboxAcceptedToHackathon = false;
-    } else {
-      this.checkboxAcceptedToHackathon = true;
+  changeAcceptedFilter(filter: string) {
+    if (filter === 'All') {
+      this.acceptedFilter = 'All';
     }
+    if (filter === 'Accepted to Hackathon') {
+      this.acceptedFilter = 'Accepted to Hackathon';
+    }
+    if (filter === 'Not Accepted to Hackathon') {
+      this.acceptedFilter = 'Not Accepted to Hackathon';
+    }
+    this.filterUserEntries();
+  }
+
+  filterUserEntries() {
+    //Show the selected Admin Filter.
+    switch(this.adminFilter) {
+      case 'All Members': {
+        this.users = this.fullUserArray;
+        break;
+      }
+      case 'Non-Admin': {
+        this.users = this.fullUserArray.filter( u => u.adminLevel == 0);
+        break;
+      }
+      case 'Admin': {
+        this.users = this.fullUserArray.filter( u => u.adminLevel == 1);
+        break;
+      }
+      case 'Super Admin': {
+        this.users = this.fullUserArray.filter( u => u.adminLevel == 2);
+        break;
+      }
+    }
+    
+    //Show the selected Application Filter.
+    switch(this.applicationFilter) {
+      case 'Has Application': {
+        this.users = this.users.filter( u => u.application != null );
+        break;
+      }
+      case 'Missing Application': {
+        this.users = this.users.filter( u => u.application == null );
+        break;
+      }
+    }
+
+    //Show the selected Accepted Filter.
+    switch(this.acceptedFilter) {
+      case 'Accepted to Hackathon': {
+        this.users = this.users.filter( u => u.isRegisteredFor2020 == true );
+        break;
+      }
+      case 'Not Accepted to Hackathon': {
+        this.users = this.users.filter( u => u.isRegisteredFor2020 == false );
+        break;
+      }
+    }
+
   }
 }
