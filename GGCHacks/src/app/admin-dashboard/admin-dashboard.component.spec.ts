81a6f1d4cb826ea2001c02258dfa57ff4c8a6928
Jordan Moats <jmoats@ggc.edu>
2023-04-24 10:31:22 -0400

    final testing updates

--- GGCHacks/src/app/admin-dashboard/admin-dashboard.component.spec.ts
+++ GGCHacks/src/app/admin-dashboard/admin-dashboard.component.spec.ts
@@ -1,18 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BlogHomeComponent } from 'src/app/blog/blog-home/blog-home.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { BlogService, Blog } from 'src/app/services/blog.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { of } from 'rxjs';
 
-import { AdminDashboardComponent } from './admin-dashboard.component';
+class MockAuthService {}
+class MockBlogService {
+  blogs$ = of<Blog[]>([]);
+}
+class MockNgbModal {}
 
-describe('AdminDashboardComponent', () => {
-  let component: AdminDashboardComponent;
-  let fixture: ComponentFixture<AdminDashboardComponent>;
+describe('BlogHomeComponent', () => {
+  let component: BlogHomeComponent;
+  let fixture: ComponentFixture<BlogHomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AdminDashboardComponent ]
-    })
-    .compileComponents();
+      declarations: [BlogHomeComponent],
+      providers: [
+        { provide: AuthService, useClass: MockAuthService },
+        { provide: BlogService, useClass: MockBlogService },
+        { provide: NgbModal, useClass: MockNgbModal },
+      ],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(AdminDashboardComponent);
+    fixture = TestBed.createComponent(BlogHomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- GGCHacks/src/app/blog/blog-home/blog-home.component.spec.ts
+++ GGCHacks/src/app/blog/blog-home/blog-home.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BlogHomeComponent } from './blog-home.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { BlogService, Blog } from 'src/app/services/blog.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { of } from 'rxjs';
+
+class MockAuthService {}
+class MockBlogService {
+  blogs$ = of<Blog[]>([]);
+}
+class MockNgbModal {}
 
 describe('BlogHomeComponent', () => {
   let component: BlogHomeComponent;
@@ -8,9 +17,13 @@ describe('BlogHomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BlogHomeComponent ]
-    })
-    .compileComponents();
+      declarations: [BlogHomeComponent],
+      providers: [
+        { provide: AuthService, useClass: MockAuthService },
+        { provide: BlogService, useClass: MockBlogService },
+        { provide: NgbModal, useClass: MockNgbModal },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(BlogHomeComponent);
     component = fixture.componentInstance;
