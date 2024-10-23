10b964efb77aadbb76003aa4b1be89962ba22aa0
Kenny <kamador2@ggc.edu>
2024-10-22 20:35:22 -0400

    Removed application from the header as it was now of the user dashboard and also changed the requirements for the user and admin dashboards to be displayed

---
+++ GGCHacks/src/app/project-submission/project-submission.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-submission',
+  templateUrl: './project-submission.component.html',
+  styleUrls: ['./project-submission.component.css']
+})
+export class ProjectSubmissionComponent {
+
+}
